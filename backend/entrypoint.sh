348990dc460a729ecba91e9b87c006c071d1b209
shota <s8152.45@gmail.com>
2024-03-13 17:14:18 +0900

    Change: 別の記事を参考に作り直した
    構造を整えた

--- backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Railsサーバーが前回の起動時に生成したserver.pidファイルを削除します。
-# これはサーバーが正常に起動するために必要です。
-rm -f /app/backend/tmp/pids/server.pid
-
-# コンテナのメインプロセスを実行します。(DockerfileのCMDに設定されています。)
-exec "$@"
--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">こんにちは</div>
-  );
-}
-
-export default App;
