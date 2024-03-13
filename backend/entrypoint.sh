6f834d0939131dcb9d27312d5ff3004cf78ed5dd
shota <s8152.45@gmail.com>
2024-03-13 11:21:26 +0900

    Fix:server.pidの場所の修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Railsサーバーが前回の起動時に生成したserver.pidファイルを削除します。
 # これはサーバーが正常に起動するために必要です。
-rm -f /app/tmp/pids/server.pid
+rm -f /app/backend/tmp/pids/server.pid
 
 # コンテナのメインプロセスを実行します。(DockerfileのCMDに設定されています。)
 exec "$@"
