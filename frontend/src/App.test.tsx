35e01689619d8867a489784549297bf5930d0d80
shota <s8152.45@gmail.com>
2024-03-21 15:19:35 +0900

    Refactor: createStoreをconfigureStoreに置き換え
    Redux Toolkitの使用により、非推奨のcreateStore関連警告を解消し、アプリケーションのパフォーマンスとメンテナンス性を向上させるため。

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// configureStoreをインポート
+import { configureStore } from '@reduxjs/toolkit';
 import App from './App';
 
 interface Action {
@@ -9,26 +10,25 @@ interface Action {
   payload?: any; // ペイロードの型が何であるかに応じて、anyの部分を適切な型に変更する
 }
 
+// testReducerはそのままでOK
 const testReducer = (state = {}, action: Action) => {
   switch (action.type) {
-    // アクションのタイプに応じた処理をここに記述
     default:
       return state;
   }
 };
 
-// テスト用のReduxストアを作成
-const testStore = createStore(testReducer);
+// createStoreの代わりにconfigureStoreを使用してテスト用のストアを作成
+const testStore = configureStore({
+  reducer: testReducer,
+});
 
 test('renders learn react link', () => {
-  // <Provider>で<App />をラップして、テスト用のストアを提供
   render(
     <Provider store={testStore}>
       <App />
     </Provider>
   );
-  // ここで、Appコンポーネント内に存在する特定のテキストを検索
-  // このテキストがコンポーネントの期待される振る舞いとして正しくレンダリングされているかテストします
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
