59a53edc63024140f8e8d1d6728b8e7a0c007800
shota <s8152.45@gmail.com>
2024-04-01 15:22:18 +0900

    Remove: ファイルの削除
     Nextjsの導入に伴って

--- before/frontend/src/components/SignUp.js
+++
@@ -1,5 +0,0 @@
-const SignUp = () => {
-  return <h1>ユーザ登録</h1>;
-};
-
-export default SignUp;
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -11,12 +11,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <header>Header</header>
+      {children}
+      <footer>Footer</footer>
     </html>
   );
 }
