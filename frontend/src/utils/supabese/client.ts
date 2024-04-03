8476458b6425aebfb2c322ee58790409f73e1073
shota <s8152.45@gmail.com>
2024-04-03 16:16:34 +0900

    Add: Tailwind CSS設定追加
    src/utilsディレクトリ内のJavaScript, TypeScript, React(JSX, TSX), MDXファイルをTailwind CSSプロセス対象に追加。
    
    これにより、ユーティリティファイル内でのTailwind CSSの使用が可能に。

--- frontend/src/utils/supabese/client.ts
+++ frontend/src/utils/supabese/client.ts
@@ -1,12 +1,10 @@
-// 以前はSupabaseのクライアントを直接インポートしていましたが、
-// import { createClient } from "@supabase/supabase-js";
-// Supabase SSR用のクライアントに変更しました。
-// これにより、ブラウザサイドでの動作を最適化し、サーバーサイドレンダリング時の認証処理を強化します。
+// Supabaseクライアントを初期化し、環境変数からSupabaseのURLとANON KEYを取得
+// Initialize the Supabase client and retrieve Supabase URL and ANON KEY from environment variables
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  // createBrowserClientを使用してSupabaseのクライアントを作成し、
-  // 環境変数からURLとANON KEYを取得します。
+  // SupabaseのURLとANON KEYを環境変数から取得して、クライアントを作成
+  // Retrieve the Supabase URL and ANON KEY from environment variables to create the client
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
