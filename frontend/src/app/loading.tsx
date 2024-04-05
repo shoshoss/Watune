a40c97756d903121e31a8d2a77ffad30ce11fb55
shota <s8152.45@gmail.com>
2024-04-06 08:27:32 +0900

    Add: loading画面の追加
    データ取得中の画面表示させるため

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,3 +1,10 @@
-export default function Loading() {
-  return <div>Loading...</div>;
-}
+// ローディング
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="h-7 w-7 animate-spin rounded-full border-2 border-sky-500 border-t-transparent" />
+    </div>
+  );
+};
+
+export default Loading;
