3bd8d0020951c3897cf2d439fbdf299ce8576b61
shota <s8152.45@gmail.com>
2024-04-06 10:29:07 +0900

    Fix: メインページ
    createServerComponentClientを使用して、サーバー側で認証状態を取得します。
    セッションが取得できれば、ログイン状態となります。
    セッションが取得できなければ、未ログイン状態となります。

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,24 @@
-import LoginPage from "./login/page";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export default async function Index() {
+import type { Database } from "@/lib/database.types";
+
+// メインページ
+const Home = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  });
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
-    <div>
-      <nav>Supabase SSR 認証でのログイン</nav>
+    <div className="text-center text-xl">
+      {session ? <div>ログイン済</div> : <div>未ログイン</div>}
     </div>
   );
-}
+};
+
+export default Home;
