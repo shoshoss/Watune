5902633dd37e12a84f6d0e7d86755e0c624c7cef
shota <s8152.45@gmail.com>
2024-04-06 10:12:18 +0900

    Add: Supabase テーブルの型をインポート

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
