911726a34313e886cdfbc893d6b98b26527fb760
shota <s8152.45@gmail.com>
2024-04-09 09:11:59 +0900

    Add: ルーティングとビューの設定
    config/routes.rbにrender/indexへのルートを追加し、rootパスを設定しました。
    
    app/views/render/index.html.erbにHello Worldのビューを追加し、localhost:3000での表示を確認しました。この変更により、アプリケーションの基本的なレスポンスとしてHello Worldが表示されるようになります。

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root "render#index"
   # root "posts#index"
 end
