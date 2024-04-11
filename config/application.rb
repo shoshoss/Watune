2137c6c3f530c89eec0209d577e26d775c2202e1
shota <s8152.45@gmail.com>
2024-04-11 11:27:39 +0900

    Fix: rubocop -A .rubocop_todo.ymlと.rubocop.yml

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ require 'action_cable/engine'
 Bundler.require(*Rails.groups)
 
 module App
+  # アプリケーション設定クラス
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
