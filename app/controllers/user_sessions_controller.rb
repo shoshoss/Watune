8b968d22a362d7c873e2beafd2ea00ca96cbde00
shota <s8152.45@gmail.com>
2024-04-16 09:27:18 +0900

    Add: 国際化の無効化
    必要に応じて後に行う

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,21 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
   def new; end
 
-  def create; end
+  def create
+    @user = login(params[:email], params[:password])
 
-  def destroy; end
+    if @user
+      redirect_back_or_to(root_path, notice: 'ログインしました。')
+    else
+      flash.now[:alert] = 'ログインに失敗しました。'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, status: :see_other
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
