6658d53d3d31de18484a5ec3e6d938cebed28ea4
shota <s8152.45@gmail.com>
2024-04-17 09:22:51 +0900

    Add: フラッシュメッセージ、daisyUI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(user_params[:email], user_params[:password])
-      redirect_to edit_profile_path
+      redirect_to edit_profile_path, notice: 'ユーザー登録に成功しました'
     else
+      flash.now[:alert] = 'ユーザー登録に失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
