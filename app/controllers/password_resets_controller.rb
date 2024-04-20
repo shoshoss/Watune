955e60113fb76e070a883afe7313a0ff8be5d903
shota <s8152.45@gmail.com>
2024-04-21 06:06:29 +0900

    Add: パスワードリセットを処理するコントローラー
    User.load_from_reset_password_token(params[:id])にて、有効期限切れなどでユーザーが取得できなかったとき、not_authenticated
    
    not_authenticatedは次に定義する

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,42 @@
+class PasswordResetsController < ApplicationController
+  skip_before_action :require_login
+
+  # パスワードリセット申請画面へレンダリングするアクション
+  def new; end
+
+  # パスワードのリセットを要求するアクション。
+  # ユーザーがパスワードのリセットフォームにメールアドレスを入力して送信すると、このアクションが実行される。
+  def create
+    @user = User.find_by(email: params[:email])
+    # この行は、パスワード（ランダムトークンを含むURL）をリセットする方法を説明した電子メールをユーザーに送信します
+    @user&.deliver_reset_password_instructions!
+    # 「存在しないメールアドレスです」という旨の文言を表示すると、逆に存在するメールアドレスを特定されてしまうため、
+    # あえて成功時のメッセージを送信させている
+    redirect_to login_path, status: :see_other, success: t('.success')
+  end
+
+  # パスワードのリセットフォーム画面へ遷移するアクション
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+    not_authenticated if @user.blank?
+  end
+
+  # ユーザーがパスワードのリセットフォームを送信したときに発生
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+
+    return not_authenticated if @user.blank?
+
+    # 次の行は、パスワード確認の検証を機能させます
+    @user.password_confirmation = params[:user][:password_confirmation]
+    # 次の行は一時トークンをクリアし、パスワードを更新します
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, status: :see_other, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+end
---
+++ spec/routing/password_resets_routing_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'PasswordResetsController', type: :routing do
+  describe 'routing' do
+    it 'routes to #new' do
+      expect(get: "/password_resets/new").to route_to("password_resets#new")
+    end
+    it 'routes to #create' do
+      expect(get: "/password_resets/create").to route_to("password_resets#create")
+    end
+    it 'routes to #edit' do
+      expect(get: "/password_resets/edit").to route_to("password_resets#edit")
+    end
+    it 'routes to #update' do
+      expect(get: "/password_resets/update").to route_to("password_resets#update")
+    end
+  end
+end
