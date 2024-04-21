47e0bff6c3563acbf7172d53098e9cba3e1a2a7f
shota <s8152.45@gmail.com>
2024-04-21 12:02:20 +0900

    Update: oauths_controllerのリファクタリングとステータスコードの指定
    POSTメソッドのリクエストは303で重複するPOSTリクエストを許可しないようにした。303はリダイレクト先でのリクエストを明確に GET メソッドに変更する。
    
    GETリクエストの場合は302

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,62 +1,58 @@
 class OauthsController < ApplicationController
-  # ログインが必要ないアクションを設定
+  # ログインが不要なアクションについてはログイン要求をスキップ
   skip_before_action :require_login
 
-  # OAuth認証プロセスを開始するアクション
+  # OAuth認証を開始するアクション
   def oauth
-    # パラメータからプロバイダーを取得またはデフォルトで 'google' を設定
+    # プロバイダーをパラメータから取得、デフォルトは 'google'
     provider = params[:provider] || 'google'
-    # Sorceryのlogin_atメソッドを用いてプロバイダーの認証ページにリダイレクト
+    # Sorceryのlogin_atメソッドで認証ページにリダイレクト
     login_at(provider.to_sym)
-    # セッションにプロバイダーを保存
+    # プロバイダーをセッションに保存
     session[:provider] = provider
   end
 
   # OAuth認証からのコールバックを処理するアクション
   def callback
-    # セッションからプロバイダーを取得し、削除する
+    # セッションからプロバイダーを取得し、削除
     provider = session.delete(:provider) || 'google'
     # プロバイダーからのデータを取得
     sorcery_fetch_user_hash(provider)
 
-    # 既存のユーザーとしてログインを試みる
+    # ユーザーでログインを試みる
     @user = login_from(provider)
     if @user
-      # ログインに成功した場合はトップページにリダイレクト
+      # ログイン成功時、トップページにリダイレクト
       redirect_to root_path, status: :see_other, notice: "#{provider.titleize} アカウントでログインしました"
     else
-      # ユーザーが見つからない場合は、ユーザーを検索または初期化
+      # ユーザーが見つからない場合、新規作成または初期化
       @user = find_or_initialize_user(provider)
-      # 新規ユーザーかどうかを判定
       is_new_user = @user.new_record?
-      # 新規ユーザーの場合、ユーザー情報の設定と保存
+      # 新規ユーザーの場合、ユーザー情報を設定
       setup_new_user(@user) if is_new_user
       # セッションをリセットし、ユーザーでログイン
       reset_session
       auto_login(@user)
-      # リダイレクト先と通知メッセージを設定
-      redirect_path, notice_message = if is_new_user
-                                        [edit_profile_path, 'アカウントでユーザー登録に成功しました']
-                                      else
-                                        [root_path, 'アカウントでログインしました']
-                                      end
-      # リダイレクト実行
-      redirect_to redirect_path, status: :see_other, notice: "#{provider.titleize} #{notice_message}"
+      # リダイレクト先と通知メッセージを決定
+      redirect_path, notice_message = determine_redirect(is_new_user, provider)
+      # 最終的なリダイレクトを実行
+      redirect_to redirect_path, status: :see_other, notice: notice_message
     end
   rescue ActiveRecord::RecordInvalid => e
-    # レコードの保存に失敗した場合はログインページにリダイレクト
-    redirect_to login_path, status: :unprocessable_entity, error: "アカウントの作成に失敗しました。エラー: #{e.message}"
+    # 保存失敗時はログインページにリダイレクトしエラーを表示
+    flash[:error] = "アカウントの作成に失敗しました。エラー: #{e.message}"
+    redirect_to login_path, status: :unprocessable_entity
   end
 
   private
 
-  # プロバイダーからの情報に基づいてユーザーを検索または初期化するメソッド
+  # プロバイダーからの情報に基づいてユーザーを検索または初期化
   def find_or_initialize_user(_provider)
     email = @user_hash[:user_info]['email']
     User.find_or_initialize_by(email:)
   end
 
-  # 新規ユーザーの設定と保存を行うメソッド
+  # 新規ユーザーの設定と保存を行う
   def setup_new_user(user)
     user.assign_attributes(
       display_name: @user_hash[:user_info]['name'].presence || 'Default Name',
@@ -64,5 +60,14 @@ class OauthsController < ApplicationController
     )
     user.password = SecureRandom.alphanumeric(10)
     user.save!
+  end
+
+  # リダイレクト先と通知メッセージを決定する
+  def determine_redirect(is_new_user, provider)
+    if is_new_user
+      [edit_profile_path, "#{provider.titleize} アカウントでユーザー登録に成功しました"]
+    else
+      [root_path, "#{provider.titleize} アカウントでログインしました"]
+    end
   end
 end
