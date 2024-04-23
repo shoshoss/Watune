78da466a73a8b11065279313448e9635438ff717
shota <s8152.45@gmail.com>
2024-04-23 17:10:02 +0900

    Update: daisyUIでモーダル

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to root_path, status: :see_other, notice: 'ログインに成功しました'
+      redirect_to root_path, status: :see_other, notice: 'ログインしました'
     else
       flash.now[:error] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
