740c8100329067a3929f5b0d97c8c1e90d476960
shota <s8152.45@gmail.com>
2024-05-02 09:16:54 +0900

    録音の修正

--- app/javascript/controllers/audio_modal_controller.js
+++ app/javascript/controllers/audio_modal_controller.js
@@ -1,27 +1,122 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  async connect() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      this.recorder = new MediaRecorder(stream);
-      this.recorder.ondataavailable = (event) => {
-        const audioBlob = new Blob([event.data], { type: "audio/wav" });
-        const audioUrl = URL.createObjectURL(audioBlob);
-        this.element.querySelector("audio").src = audioUrl;
-      };
-    } catch (error) {
-      console.error("Error accessing the microphone: ", error);
+  audioCtx;
+  canvasCtx;
+  mediaRecorder;
+  shouldStop = false;
+  animationFrameRequest;
+
+  connect() {
+    this.element.setAttribute("open", true); // モーダルを開く処理
+    this.canvasCtx = this.element.querySelector(".visualizer").getContext("2d");
+    if (!this.audioCtx) {
+      this.audioCtx = new AudioContext();
     }
   }
 
+  closeModal() {
+    this.element.close(); // モーダルを閉じる
+  }
+
   startRecording() {
-    this.recorder.start();
-    console.log("録音を開始しました。");
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        this.mediaRecorder = new MediaRecorder(stream);
+        let chunks = [];
+
+        this.element.querySelector(".record").disabled = true;
+        this.element.querySelector(".stop").disabled = false;
+        this.shouldStop = false;
+
+        this.mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
+        this.mediaRecorder.onstop = (e) => this.handleRecordingStop(chunks);
+        this.mediaRecorder.start();
+
+        this.visualize(stream);
+      })
+      .catch((error) => console.log("Error accessing the microphone:", error));
   }
 
   stopRecording() {
-    this.recorder.stop();
-    console.log("録音を停止しました。");
+    this.mediaRecorder.stop();
+    this.element.querySelector(".record").disabled = false;
+    this.element.querySelector(".stop").disabled = true;
+    this.shouldStop = true;
+    cancelAnimationFrame(this.animationFrameRequest);
+    console.log("Recorder stopped and visualization stopped.");
+  }
+
+  handleRecordingStop(chunks) {
+    const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+    const audioURL = window.URL.createObjectURL(blob);
+    this.createSoundClip(audioURL);
+  }
+
+  createSoundClip(audioURL) {
+    const clipContainer = document.createElement("article");
+    const audio = document.createElement("audio");
+    const deleteButton = document.createElement("button");
+
+    clipContainer.classList.add("clip");
+    audio.setAttribute("controls", "");
+    audio.src = audioURL;
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete";
+
+    clipContainer.appendChild(audio);
+    clipContainer.appendChild(deleteButton);
+    this.element.querySelector(".sound-clips").appendChild(clipContainer);
+
+    deleteButton.addEventListener("click", (e) => {
+      e.target.closest(".clip").remove();
+    });
+  }
+
+  visualize(stream) {
+    const source = this.audioCtx.createMediaStreamSource(stream);
+    const analyser = this.audioCtx.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    source.connect(analyser);
+
+    const WIDTH = this.element.querySelector(".visualizer").width;
+    const HEIGHT = this.element.querySelector(".visualizer").height;
+
+    const draw = () => {
+      if (this.shouldStop) return;
+      this.animationFrameRequest = requestAnimationFrame(draw);
+      analyser.getByteTimeDomainData(dataArray);
+
+      this.canvasCtx.fillStyle = "rgb(200, 200, 200)";
+      this.canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      this.canvasCtx.lineWidth = 2;
+      this.canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+      this.canvasCtx.beginPath();
+
+      let sliceWidth = (WIDTH * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        let v = dataArray[i] / 128.0;
+        let y = (v * HEIGHT) / 2;
+
+        if (i === 0) {
+          this.canvasCtx.moveTo(x, y);
+        } else {
+          this.canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      this.canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+      this.canvasCtx.stroke();
+    };
+
+    draw();
   }
 }
--- app/javascript/controllers/post_modal_controller.js
+++ app/javascript/controllers/post_modal_controller.js
@@ -1,11 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["record", "stop", "canvas", "soundClips"];
+  audioCtx;
+  canvasCtx;
+
   connect() {
     // ボタンクリック時にモーダルを開く処理
     this.element.setAttribute("open", true);
     // モーダルが開いたときにバックグラウンドをクリックして閉じるイベントを追加
     // this.element.addEventListener("click", this.closeBackground.bind(this));
+
+    this.canvasCtx = this.canvasTarget.getContext("2d");
+    if (!this.audioCtx) {
+      this.audioCtx = new AudioContext();
+    }
   }
 
   closeModal() {
@@ -33,5 +42,104 @@ export default class extends Controller {
     // リダイレクトパスを取得してリダイレクトを実行する
     const redirectPath = this.data.get("redirectPath");
     window.location.href = redirectPath;
+  }
+
+  startRecording() {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        let chunks = [];
+
+        this.recordTarget.disabled = true;
+        this.stopTarget.disabled = false;
+
+        mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
+        mediaRecorder.onstop = (e) => this.handleRecordingStop(chunks);
+        mediaRecorder.start();
+
+        this.visualize(stream);
+      })
+      .catch((error) => console.log("Error accessing the microphone:", error));
+  }
+
+  stopRecording() {
+    this.recordTarget.disabled = false;
+    this.stopTarget.disabled = true;
+    console.log("stopRecording");
+  }
+
+  handleRecordingStop(chunks) {
+    const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+    const audioURL = window.URL.createObjectURL(blob);
+    this.createSoundClip(audioURL);
+  }
+
+  createSoundClip(audioURL) {
+    const clipContainer = document.createElement("article");
+    const audio = document.createElement("audio");
+    const deleteButton = document.createElement("button");
+
+    clipContainer.classList.add("clip");
+    audio.setAttribute("controls", "");
+    audio.src = audioURL;
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "delete";
+
+    clipContainer.appendChild(audio);
+    clipContainer.appendChild(deleteButton);
+    this.soundClipsTarget.appendChild(clipContainer);
+
+    deleteButton.addEventListener("click", (e) => {
+      e.target.closest(".clip").remove();
+    });
+  }
+
+  visualize(stream) {
+    const source = this.audioCtx.createMediaStreamSource(stream);
+    const analyser = this.audioCtx.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    source.connect(analyser);
+
+    const WIDTH = this.canvasTarget.width;
+    const HEIGHT = this.canvasTarget.height;
+
+    const draw = () => {
+      requestAnimationFrame(draw);
+      analyser.getByteTimeDomainData(dataArray);
+
+      this.canvasCtx.fillStyle = "rgb(200, 200, 200)";
+      this.canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      this.canvasCtx.lineWidth = 2;
+      this.canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+      this.canvasCtx.beginPath();
+
+      let sliceWidth = (WIDTH * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        let v = dataArray[i] / 128.0;
+        let y = (v * HEIGHT) / 2;
+
+        if (i === 0) {
+          this.canvasCtx.moveTo(x, y);
+        } else {
+          this.canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      this.canvasCtx.lineTo(
+        this.canvasTarget.width,
+        this.canvasTarget.height / 2
+      );
+      this.canvasCtx.stroke();
+    };
+
+    draw();
   }
 }
