3f870a9c9524cc5cad396a1c04a72a092205e486
shota <s8152.45@gmail.com>
2024-05-04 17:18:44 +0900

    Add: 再生と停止

--- app/javascript/controllers/audio_controller.js
+++ app/javascript/controllers/audio_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   playPause(event) {
-    console.log("playPause triggered");
     const button = event.currentTarget;
     const audioId = button.dataset.audioId;
     const audio = document.querySelector(`audio[id="audio-${audioId}"]`);
@@ -18,8 +17,6 @@ export default class extends Controller {
         .then(() => {
           button.classList.remove("fa-play");
           button.classList.add("fa-pause");
-          button.innerHTML =
-            '<i class="fas fa-pause text-3xl text-blue-500 hover:text-blue-700"></i>';
         })
         .catch((error) => {
           console.error("Playback failed:", error);
@@ -28,8 +25,6 @@ export default class extends Controller {
       audio.pause();
       button.classList.remove("fa-pause");
       button.classList.add("fa-play");
-      button.innerHTML =
-        '<i class="fas fa-play text-3xl text-blue-500 hover:text-blue-700"></i>';
     }
   }
 }
