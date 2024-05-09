5eda5aae18bf88a1946ec325781acb42dc957428
shota <s8152.45@gmail.com>
2024-05-09 16:58:40 +0900

    Add: 改行のテスト

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,17 @@
+module PostsHelper
+  def custom_simple_format(text, html_options = {}, options = {})
+    wrapper_tag = options.fetch(:wrapper_tag, :p)
+
+    text = sanitize(text) if options.fetch(:sanitize, true)
+    paragraphs = split_paragraphs(text)
+
+    if paragraphs.empty?
+      content_tag(wrapper_tag, nil, html_options)
+    else
+      paragraphs.map! { |paragraph|
+        content_tag(wrapper_tag, raw(paragraph), html_options)
+      }.join("\n\n").html_safe
+    end
+  end
+
+end
