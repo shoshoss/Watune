4a090ea16c57f89f8aa95cc72c348c7dec49e542
shota <s8152.45@gmail.com>
2024-05-17 16:37:22 +0900

    Add: 編集の更新(タブメニュやヘッダー、サイドバーはまだ)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,7 +8,7 @@ class ProfilesController < ApplicationController
     respond_to do |format|
       format.html
       format.turbo_stream do
-        render turbo_stream: turbo_stream.replace('posts', partial: 'profiles/posts',
+        render turbo_stream: turbo_stream.replace('profile-posts', partial: 'profiles/show_posts',
                                                            locals: { posts: @posts, pagy: @pagy })
       end
     end
@@ -21,12 +21,30 @@ class ProfilesController < ApplicationController
   def update
     @user.avatar.attach(params[:user][:avatar]) if @user.avatar.blank?
     if @user.update(user_params)
-      redirect_to profile_show_path(username_slug: @user.username_slug), status: :see_other, notice: 'プロファイルが更新されました。'
+      flash.now[:notice] = t('defaults.flash_message.updated', item: Profile.model_name.human)
+      respond_to do |format|
+        format.html { redirect_to profile_show_path(@user.username_slug), status: :see_other }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace('flash_messages', partial: 'shared/flash_message'),
+            turbo_stream.replace('profile_edit_frame', partial: 'profiles/show', locals: { user: @user, posts: @posts, pagy: @pagy })
+          ]
+        end
+      end
     else
-      flash.now[:error] = 'プロファイルの更新に失敗しました。'
-      render :edit, status: :unprocessable_entity
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: Profile.model_name.human)
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace('flash_messages', partial: 'shared/flash_message'),
+            turbo_stream.replace('error_messages_frame', partial: 'shared/error_messages', locals: { object: @user })
+          ]
+        end
+      end
     end
   end
+  
 
   private
 
