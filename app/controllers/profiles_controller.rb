c4fbab1bc3ca766a83a5eb45724bf6167c810dda
shota <s8152.45@gmail.com>
2024-05-17 18:48:51 +0900

    update.turbo_stream.erbを作成 ビューのロジックをコントローラから分離し、コードの可読性とメンテナンス性を向上させるための方法

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,34 +17,30 @@ class ProfilesController < ApplicationController
   end
 
   # プロフィール更新アクション
-  def update
-    @user.avatar.attach(params[:user][:avatar]) if @user.avatar.blank?
-    if @user.update(user_params)
-      flash.now[:notice] = t('defaults.flash_message.updated', item: Profile.model_name.human)
-      respond_to do |format|
-        format.html { redirect_to profile_show_path(@user.username_slug), status: :see_other }
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.replace('after-sidebar', partial: 'shared/sidebar'),
-            turbo_stream.replace('after-header', partial: 'shared/header'),
-            turbo_stream.replace('flash_messages', partial: 'shared/flash_message'),
-            turbo_stream.replace('profile_edit_frame', partial: 'profiles/show', locals: { user: @user, posts: @posts, pagy: @pagy })
-          ]
-        end
-      end
-    else
-      flash.now[:danger] = t('defaults.flash_message.not_updated', item: Profile.model_name.human)
-      respond_to do |format|
-        format.html { render :edit, status: :unprocessable_entity }
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.replace('flash_messages', partial: 'shared/flash_message'),
-            turbo_stream.replace('error_messages_frame', partial: 'shared/error_messages', locals: { object: @user })
-          ]
-        end
+def update
+  @user.avatar.attach(params[:user][:avatar]) if @user.avatar.blank?
+  if @user.update(user_params)
+    flash.now[:notice] = t('defaults.flash_message.updated', item: Profile.model_name.human)
+    params[:category] ||= 'self'
+    @pagy, @posts = pagy_countless(filtered_posts, items: 10)
+    respond_to do |format|
+      format.html { redirect_to profile_show_path(@user.username_slug), status: :see_other }
+      format.turbo_stream
+    end
+  else
+    flash.now[:danger] = t('defaults.flash_message.not_updated', item: Profile.model_name.human)
+    respond_to do |format|
+      format.html { render :edit, status: :unprocessable_entity }
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.replace('flash_messages', partial: 'shared/flash_message'),
+          turbo_stream.replace('error_messages_frame', partial: 'shared/error_messages', locals: { object: @user })
+        ]
       end
     end
   end
+end
+
 
   private
 
