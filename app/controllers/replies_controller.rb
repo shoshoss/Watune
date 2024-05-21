7703792457a27820eddb40853961e48a17b948c2
shota <s8152.45@gmail.com>
2024-05-21 18:34:35 +0900

    必要のないコードの削除 post.bodyの制限を戻した ja.ymlでメッセージに。をつけた

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -5,24 +5,16 @@ class RepliesController < ApplicationController
     @reply = @post.replies.build(reply_params)
     @reply.user = current_user
     if @reply.save
-      flash[:notice] = '返信が作成されました。'
+      flash[:notice] = '返信しました。'
       respond_to do |format|
         format.turbo_stream
         format.html { redirect_to user_post_path(@post.user.username_slug, @post), notice: '返信が作成されました。' }
       end
     else
-      flash.now[:danger] = '返信の作成に失敗しました。'
+      flash.now[:danger] = '返信できませんでした。'
       respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.update("error_messages", partial: "shared/error_messages", locals: { object: @reply }),
-            turbo_stream.update("flash", partial: "shared/flash_message")
-          ]
-        end
-        format.html do
-          @pagy, @replies = pagy(@post.replies.order(created_at: :desc), items: 10)
-          render 'posts/show', status: :unprocessable_entity
-        end
+        format.turbo_stream
+        format.html { render 'posts/show', status: :unprocessable_entity }
       end
     end
   end
