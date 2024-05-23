0482e8201344a60d7791f31b4ac24aea1c5ca053
shota <s8152.45@gmail.com>
2024-05-23 20:36:30 +0900

    Add: ユーザー数と投稿数が1000を超える場合に1.0kのように表示
    helperで"#{(count / 1000.0).round(1)}k" などでKとMの表示に変換

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,4 +65,15 @@ module ApplicationHelper
   def active_if(path)
     current_page?(path) ? 'font-bold border-b-4 border-sky-400 text-blue-500' : 'font-medium border-b-2 border-gray-300'
   end
+
+  # ユーザー数と投稿数を表示
+  def format_count(count)
+    if count >= 1_000_000
+      "#{(count / 1_000_000.0).round(1)}M"
+    elsif count >= 1_000
+      "#{(count / 1_000.0).round(1)}K"
+    else
+      count.to_s
+    end
+  end
 end
