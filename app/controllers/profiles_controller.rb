4d8d9ebdcf4dde13b7050640af30dceb1ebaf2f0
shota <s8152.45@gmail.com>
2024-05-25 13:42:07 +0900

    Improve: プロフィール編集成功時のフラッシュメッセージの表示

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,14 +22,17 @@ class ProfilesController < ApplicationController
 
   # プロフィール更新アクション
   def update
-    return unless @user.update(user_params)
+    return unless 
 
     if @user.display_name.blank?
       @user.update(display_name: "ウェーブ登録#{@user.id}")
       flash[:notice] = t('defaults.flash_message.updated_with_default_name', item: 'プロフィール')
       return
+    elsif @user.update(user_params)
+      flash[:notice] = t('defaults.flash_message.updated', item: 'プロフィール')
+    else
+      flash.now[:notice] = t('defaults.flash_message.updated', item: 'プロフィール')
     end
-    flash.now[:notice] = t('defaults.flash_message.updated', item: 'プロフィール')
   end
 
   # プロフィールモーダル表示アクション
