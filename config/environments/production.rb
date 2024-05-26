c3c64a3c7438a9861126e2fc3702831190a62e6d
shota <s8152.45@gmail.com>
2024-05-26 18:51:07 +0900

    Change: 位置の変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,9 +105,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts << 'wavecongra.onrender.com'
   config.hosts << 'www.wavescg.com'
   config.hosts << 'wavescg.com'
-  config.hosts << 'wavecongra.onrender.com'
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
