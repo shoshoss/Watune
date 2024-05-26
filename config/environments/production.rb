477612cb4a1b802af8d6c12b6b9d389189fcdec5
shota <s8152.45@gmail.com>
2024-05-26 18:49:26 +0900

    Add: config.hosts << wavecongra.onrender.comも追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,6 +107,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts << 'www.wavescg.com'
   config.hosts << 'wavescg.com'
+  config.hosts << 'wavecongra.onrender.com'
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
