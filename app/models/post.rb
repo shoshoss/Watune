3f2540012e6e0e8a311a43adcc7ab7c6164346e0
shota <s8152.45@gmail.com>
2024-05-26 21:51:32 +0900

    Improve: with_likes_count_all と public_likes_chance スコープを修正して、自分がいいねをした投稿を除外するように

--- app/models/post.rb
+++ app/models/post.rb
@@ -31,33 +31,34 @@ class Post < ApplicationRecord
   # 自分の投稿で自分が応援していないもの、および他のユーザーの公開設定された投稿で、
   # 応援の数が0から9の範囲に収まるものを取得するスコープ
   scope :with_likes_count_all, lambda { |user|
-    user_posts = where(user_id: user.id)
-                 .left_joins(:likes)
-                 .group('posts.id')
-                 .having('COUNT(likes.id) <= 0')
-                 .order(created_at: :asc)
+  user_posts = where(user_id: user.id)
+              .left_joins(:likes)
+              .group('posts.id')
+              .having('COUNT(likes.id) <= 0')
+              .order(created_at: :asc)
 
-    # 他のユーザーの公開設定された投稿で投稿者本人の応援を除外して応援の数が0から9のものを取得
-    open_posts = where(privacy: 'open')
-                 .where.not(user_id: user.id)
-                 .left_joins(:likes)
-                 .group('posts.id')
-                 .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
-                 .order(created_at: :asc)
+  open_posts = where(privacy: 'open')
+              .where.not(user_id: user.id)
+              .left_joins(:likes)
+              .group('posts.id')
+              .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
+              .having('SUM(CASE WHEN likes.user_id = ? THEN 1 ELSE 0 END) = 0', user.id)
+              .order(created_at: :asc)
 
-    # 両方の条件を結合
-    user_posts.or(open_posts)
+  user_posts.or(open_posts)
   }
 
   # 自分以外のユーザーの公開設定された投稿を、投稿者本人の応援を除外して応援の数が0から9のものに限定して取得するスコープ
   scope :public_likes_chance, lambda { |user|
-    where.not(user_id: user.id)
-         .where(privacy: 'open')
-         .left_joins(:likes)
-         .group('posts.id')
-         .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
-         .order(created_at: :asc)
+  where.not(user_id: user.id)
+      .where(privacy: 'open')
+      .left_joins(:likes)
+      .group('posts.id')
+      .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
+      .having('SUM(CASE WHEN likes.user_id = ? THEN 1 ELSE 0 END) = 0', user.id)
+      .order(created_at: :asc)
   }
+
 
   # 自分だけの投稿を取得するスコープ
   scope :only_me, -> { where(privacy: 'only_me').order(created_at: :desc) }
