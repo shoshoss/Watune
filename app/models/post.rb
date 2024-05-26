20f8bfd60f6b1808e1814ee2e295ff09150cd0c2
shota <s8152.45@gmail.com>
2024-05-26 20:07:12 +0900

    Add: 応援チャンスの並び順を投稿順へ（最新が下へ）

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,18 +21,21 @@ class Post < ApplicationRecord
   scope :visible_to, ->(user) { where(privacy: %i[open only_yours only_friends]).or(where(user:)) }
 
   # ユーザーが応援していない投稿を取得するスコープ
-  scope :not_liked_by_user, lambda { |user|
-    where(user_id: user.id).left_joins(:likes).where(likes: { user_id: nil })
+  scope :not_liked_by_user, ->(user) {
+    where(user_id: user.id)
+    .left_joins(:likes)
+    .where(likes: { user_id: nil })
+    .order(created_at: :asc)
   }
 
   # 自分の投稿で自分が応援していないもの、および他のユーザーの公開設定された投稿で、
   # 応援の数が0から9の範囲に収まるものを取得するスコープ
-  scope :with_likes_count_all, lambda { |user|
-    # 自分の投稿で応援の数が0のものを取得
+  scope :with_likes_count_all, ->(user) {
     user_posts = where(user_id: user.id)
                  .left_joins(:likes)
                  .group('posts.id')
                  .having('COUNT(likes.id) <= 0')
+                 .order(created_at: :asc)
 
     # 他のユーザーの公開設定された投稿で投稿者本人の応援を除外して応援の数が0から9のものを取得
     open_posts = where(privacy: 'open')
@@ -40,25 +43,27 @@ class Post < ApplicationRecord
                  .left_joins(:likes)
                  .group('posts.id')
                  .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
+                 .order(created_at: :asc)
 
     # 両方の条件を結合
     user_posts.or(open_posts)
   }
 
   # 自分以外のユーザーの公開設定された投稿を、投稿者本人の応援を除外して応援の数が0から9のものに限定して取得するスコープ
-  scope :public_likes_chance, lambda { |user|
+  scope :public_likes_chance, ->(user) {
     where.not(user_id: user.id)
-         .where(privacy: 'open')
-         .left_joins(:likes)
-         .group('posts.id')
-         .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
+    .where(privacy: 'open')
+    .left_joins(:likes)
+    .group('posts.id')
+    .having('SUM(CASE WHEN likes.user_id = posts.user_id THEN 0 ELSE 1 END) <= 9')
+    .order(created_at: :asc)
   }
 
   # 自分だけの投稿を取得するスコープ
-  scope :only_me, -> { where(privacy: 'only_me') }
+  scope :only_me, -> { where(privacy: 'only_me').order(created_at: :desc) }
 
   # 公開設定された自分の投稿を取得するスコープ
-  scope :my_posts_open, -> { where(privacy: 'open') }
+  scope :my_posts_open, -> { where(privacy: 'open').order(created_at: :desc) }
 
   # 親の投稿のユーザー名が重複しないように祖先を取得するメソッド
   def ancestors
