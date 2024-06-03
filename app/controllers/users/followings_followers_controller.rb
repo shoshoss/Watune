3e6b39bef5f11c70898719c99385b8971c0f7a94
shota <s8152.45@gmail.com>
2024-06-03 10:13:57 +0900

    Change: data: { turbo_frame: "_top" }に変更
    タブのアクティブとリスト表示を確実に表示するため

--- app/controllers/users/followings_followers_controller.rb
+++ app/controllers/users/followings_followers_controller.rb
@@ -1,10 +1,18 @@
 class Users::FollowingsFollowersController < ApplicationController
+  before_action :set_user
+
   def index
+    @category = params[:category] || 'followings'
+    @pagy, @users = if @category == 'followers'
+                      pagy_countless(@user.followers, items: 15)
+                    else
+                      pagy_countless(@user.followings, items: 15)
+                    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:user_id])
-    if params[:category] == 'followers'
-      @pagy, @followers = pagy_countless(@user.followers, items: 15)
-    else
-      @pagy, @followings = pagy_countless(@user.followings, items: 15)
-    end
   end
 end
