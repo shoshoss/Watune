9fb5aa795e9e6ecfc4370df35d88f9d94570dc12
shota <s8152.45@gmail.com>
2024-06-03 09:36:29 +0900

    Add: pagyによるフォロリスト表示

--- app/controllers/users/followings_followers_controller.rb
+++ app/controllers/users/followings_followers_controller.rb
@@ -1,7 +1,10 @@
 class Users::FollowingsFollowersController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @followings = @user.followings
-    @followers = @user.followers
+    if params[:category] == 'followers'
+      @pagy, @followers = pagy_countless(@user.followers, items: 15)
+    else
+      @pagy, @followings = pagy_countless(@user.followings, items: 15)
+    end
   end
 end
