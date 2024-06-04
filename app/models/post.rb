79792f9201156c6a521689fe0b2efc7e9475e44e
shota <s8152.45@gmail.com>
2024-06-04 11:06:40 +0900

    Add: プロフィールタブの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -74,6 +74,12 @@ class Post < ApplicationRecord
   # 公開設定された自分の投稿を取得するスコープ
   scope :my_posts_open, -> { where(privacy: 'open').order(created_at: :desc) }
 
+  # 仲間への投稿を取得するスコープ
+  scope :my_posts_following, ->(user) { joins(:post_users).where(post_users: { user_id: user.following_ids, role: 'direct_recipient' }).order(created_at: :desc) }
+
+  # 仲間からの投稿を取得するスコープ
+  scope :all_from_following, ->(user) { joins(:post_users).where(post_users: { user_id: user.following_ids, role: 'direct_recipient' }).order(created_at: :desc) }
+
   # 親の投稿のユーザー名が重複しないように祖先を取得するメソッド
   def ancestors
     parents = []
