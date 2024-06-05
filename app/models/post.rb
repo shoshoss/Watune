a1d17bd7a26b008e355207bae30a85a9f98c3fb6
shota <s8152.45@gmail.com>
2024-06-05 18:26:39 +0900

    Fix: あなたと共有で返信も表示できるように

--- app/models/post.rb
+++ app/models/post.rb
@@ -94,10 +94,16 @@ class Post < ApplicationRecord
 
   # 相互のダイレクトメッセージや複数選択の送受信、返信のやり取りを表示するスコープ
   scope :shared_with_you, ->(current_user, profile_user) {
-    direct_posts = joins(:post_users).where(post_users: { user_id: [current_user.id, profile_user.id], approved: true })
-    reply_posts = where(post_reply_id: Post.where(user_id: [current_user.id, profile_user.id]).select(:id))
-
-    Post.where(id: direct_posts.select(:id)).or(Post.where(id: reply_posts.select(:id))).distinct.order(created_at: :desc)
+    direct_to_profile_user = joins(:post_users).where(user_id: current_user.id, post_users: { user_id: profile_user.id, role: 'direct_recipient', approved: true })
+    direct_to_current_user = joins(:post_users).where(user_id: profile_user.id, post_users: { user_id: current_user.id, role: 'direct_recipient', approved: true })
+    reply_from_current_user = where(user_id: current_user.id, post_reply_id: Post.where(user_id: profile_user.id).select(:id))
+    reply_from_profile_user = where(user_id: profile_user.id, post_reply_id: Post.where(user_id: current_user.id).select(:id))
+    
+    Post.where(id: direct_to_profile_user.select(:id))
+        .or(Post.where(id: direct_to_current_user.select(:id)))
+        .or(Post.where(id: reply_from_current_user.select(:id)))
+        .or(Post.where(id: reply_from_profile_user.select(:id)))
+        .distinct.order(created_at: :desc)
   }
 
   # 親の投稿のユーザー名が重複しないように祖先を取得するメソッド
