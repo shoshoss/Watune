e8e1f0637a9f7c6ea09787611f0f966f1a3d4dc6
shota <s8152.45@gmail.com>
2024-06-07 16:31:00 +0900

    Add: 通知のタブ化

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,12 @@
 class NotificationsController < ApplicationController
   def index
-    @pagy, @notifications = pagy_countless(current_user.received_notifications.unread.order(created_at: :desc), items: 15)
+    @category = params[:category] || 'all'
+    if @category == 'friends'
+      notifications_scope = current_user.received_notifications.unread.where(action: ['reply', 'post'])
+    else
+      notifications_scope = current_user.received_notifications.unread
+    end
+    @pagy, @notifications = pagy_countless(notifications_scope.includes(:sender, :notifiable).order(created_at: :desc), items: 10)
   end
 
   def update
