d9833285579859c2528d8036205b20b62d3500ff
shota <s8152.45@gmail.com>
2024-06-08 13:50:56 +0900

    Add: タブを未読と既読に分けた

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,20 @@
 class NotificationsController < ApplicationController
   def index
-    @category = params[:category] || 'all'
-    if @category == 'friends'
-      notifications_scope = current_user.received_notifications.unread.where(action: ['reply', 'post'])
-    else
-      notifications_scope = current_user.received_notifications.unread
-    end
+    @category = params[:category] || 'unread'
+    notifications_scope = case @category
+                          when 'unread'
+                            current_user.received_notifications.unread
+                          when 'friends'
+                            current_user.received_notifications.where(action: ['reply', 'post'], unread: false)
+                          when 'likes_and_follows'
+                            current_user.received_notifications.where(action: ['like', 'follow'], unread: false)
+                          else
+                            current_user.received_notifications
+                          end
+
     @pagy, @notifications = pagy_countless(notifications_scope.includes(:sender, :notifiable).order(created_at: :desc), items: 10)
 
     # 未読通知を既読にする
-    notifications_scope.update_all(unread: false)
+    notifications_scope.update_all(unread: false) if @category == 'unread'
   end
-
 end
