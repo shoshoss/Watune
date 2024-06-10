ca4fe4f7ed36a02396db8fec4f1fdc2a9d76648a
shota <s8152.45@gmail.com>
2024-06-10 17:49:58 +0900

    rubocop

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,14 +9,14 @@ class UserMailer < ApplicationMailer
     @user = user
     @reply = reply
     @url = notifications_url
-    mail(to: @user.email, subject: '新しい返信があります')
+    mail(to: @user.email, subject: '【WaveConGra】新しい通知（返信）の確認')
   end
 
   def direct_notification(user, post)
     @user = user
     @post = post
     @url = notifications_url
-    mail(to: @user.email, subject: '新しいダイレクトメッセージがあります')
+    mail(to: @user.email, subject: '【WaveConGra】新しい通知（DM）の確認')
   end
 
   private
--- app/models/concerns/posts/notification.rb
+++ app/models/concerns/posts/notification.rb
@@ -46,12 +46,12 @@ module Posts
 
       # 投稿の通知を作成するメソッド
       def create_notification_post(current_user)
-        recipients = self.post_users.where(role: 'direct_recipient').pluck(:user_id)
+        recipients = post_users.where(role: 'direct_recipient').pluck(:user_id)
         recipients.each do |recipient_id|
           next if recipient_id == current_user.id # 自分自身への通知は不要
 
           notification = current_user.sent_notifications.new(
-            recipient_id: recipient_id, # 通知の受信者
+            recipient_id:, # 通知の受信者
             sender_id: current_user.id, # 通知の送信者
             notifiable: self, # 投稿
             action: 'direct', # アクションタイプ
