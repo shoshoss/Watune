0f296f98dbe672fc5b451c8691a62043ed42981b
shota <s8152.45@gmail.com>
2024-06-11 08:16:54 +0900

    Add: 汎用的な通知ジョブを作成
    NotificationJobを生成し、汎用的な通知処理を実装。
    
    通知タイプと投稿IDを受け取り、適切な通知を送信する。

---
+++ app/jobs/notification_job.rb
@@ -0,0 +1,21 @@
+# app/jobs/notification_job.rb
+class NotificationJob < ApplicationJob
+  queue_as :default
+
+  # 通知タイプと投稿IDを受け取る
+  def perform(notification_type, post_id)
+    post = Post.find(post_id)
+    case notification_type
+    when 'reply'
+      post.create_notification_reply(post.user)
+      UserMailer.reply_notification(post.parent_post.user, post).deliver_later
+    when 'direct'
+      post.create_notification_post(post.user)
+      post.post_users.each do |post_user|
+        UserMailer.direct_notification(post_user.user, post).deliver_later
+      end
+    else
+      Rails.logger.error "Unknown notification type: #{notification_type}"
+    end
+  end
+end
