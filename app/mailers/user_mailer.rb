875e0214386190b60ce3212f7705584889c76350
shota <s8152.45@gmail.com>
2024-06-10 17:26:17 +0900

    Add: メール通知設定 環境ごとのdefault_url_optionsを使用してURLを生成するために、url_forを使用

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,27 @@
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find(user.id)
-    @url  = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email,
-         subject: t('defaults.password_reset'))
+    @url = edit_password_reset_url(@user.reset_password_token)
+    mail(to: user.email, subject: t('defaults.password_reset'))
   end
 
   def reply_notification(user, reply)
     @user = user
     @reply = reply
+    @url = notifications_url
     mail(to: @user.email, subject: '新しい返信があります')
   end
 
-  def direct_notification(user, message)
+  def direct_notification(user, post)
     @user = user
-    @message = message
+    @post = post
+    @url = notifications_url
     mail(to: @user.email, subject: '新しいダイレクトメッセージがあります')
+  end
+
+  private
+
+  def notifications_url
+    url_for(controller: 'notifications', action: 'index', only_path: false)
   end
 end
