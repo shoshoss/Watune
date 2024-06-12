98af6abcd1c6ccfcd8a75b5297431558dedc0096
shota <s8152.45@gmail.com>
2024-06-12 19:04:12 +0900

    Add: notification_emailが設定されている場合はそのメールアドレスを使用。設定されていない場合はデフォルトの@user.email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,21 +2,21 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email, subject: t('defaults.password_reset'))
+    mail(to: user.notification_email.presence || user.email, subject: t('defaults.password_reset'))
   end
 
   def reply_notification(user, reply)
     @user = user
     @reply = reply
     @url = notifications_url
-    mail(to: @user.email, subject: '【WaveConGra】新しい通知（返信）の確認')
+    mail(to: @user.notification_email.presence || @user.email, subject: '【WaveConGra】新しい通知（返信）の確認')
   end
 
   def direct_notification(user, post)
     @user = user
     @post = post
     @url = notifications_url
-    mail(to: @user.email, subject: '【WaveConGra】新しい通知（DM）の確認')
+    mail(to: @user.notification_email.presence || @user.email, subject: '【WaveConGra】新しい通知（DM）の確認')
   end
 
   private
