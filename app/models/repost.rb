33e407139f214e6fe92695a42fbf32073dd853b2
shota <s8152.45@gmail.com>
2024-06-13 08:05:33 +0900

    Add: Repostモデルの作成
    Repostモデルにアソシエーションとバリデーションを追加

--- app/models/repost.rb
+++ app/models/repost.rb
@@ -1,5 +1,7 @@
 class Repost < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  belongs_to :original_post
+  belongs_to :original_post, class_name: 'Post'
+  
+  validates :body, length: { maximum: 10_000 }, allow_nil: true
 end
