f98959095f6d4a89a5a9c7e3f6da11d4d62a2e79
shota <s8152.45@gmail.com>
2024-06-13 08:07:23 +0900

    Update: RepostsControllerの更新
    通常のリポストと引用リポストの作成ロジックを追加
    
    リポストが一度しかできないようにバリデーションを追加

--- app/controllers/reposts_controller.rb
+++ app/controllers/reposts_controller.rb
@@ -2,15 +2,22 @@ class RepostsController < ApplicationController
   before_action :set_post
 
   def create
-    if Repost.exists?(user: current_user, post: @post)
-      redirect_to root_path, alert: '既にリポスト済みです'
+    if params[:body].present?
+      # 引用リポストの作成
+      @repost = Repost.new(user: current_user, post: @post, original_post: @post, body: params[:body])
     else
-      @repost = Repost.create(user: current_user, post: @post, original_post: @post, body: params[:body])
-      if @repost.save
-        redirect_to root_path, notice: 'リポストしました'
-      else
-        redirect_to root_path, alert: 'リポストに失敗しました'
+      # 通常のリポストの作成
+      if Repost.exists?(user: current_user, post: @post)
+        redirect_to root_path, alert: '既にリポスト済みです'
+        return
       end
+      @repost = Repost.new(user: current_user, post: @post, original_post: @post)
+    end
+
+    if @repost.save
+      redirect_to root_path, notice: 'リポストしました'
+    else
+      redirect_to root_path, alert: 'リポストに失敗しました'
     end
   end
 
