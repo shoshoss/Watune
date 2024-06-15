ed9c01cb1b07dbf4c732839e27d201acf3edd6bf
shota <s8152.45@gmail.com>
2024-06-16 05:57:00 +0900

    Add: initializers/ogp.rbの作成とアバター表示のCSS修正

---
+++ config/initializers/ogp.rb
@@ -0,0 +1,7 @@
+# config/initializers/ogp.rb
+Rails.application.config.opengraph = {
+  title: 'Watune',
+  description: 'このWebアプリは、音声による前向きなメッセージを通じて、仲間と喜びや元氣を分かち合い、前向きなエネルギーを共有するサービスです。',
+  image: 'https://www.watune.com/path_to_image.png',
+  url: 'https://www.watune.com'
+}
