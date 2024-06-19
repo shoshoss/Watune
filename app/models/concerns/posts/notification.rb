eae874b557778354578cda316627067ce411f97d
shota <s8152.45@gmail.com>
2024-06-19 12:19:50 +0900

    endの忘れ

--- app/models/concerns/posts/notification.rb
+++ app/models/concerns/posts/notification.rb
@@ -53,24 +53,28 @@ module Posts
 
       # 返信の通知を作成するメソッド
       def create_notification_reply(current_user)
-        recipient_id = parent_post.user_id
-        return if current_user.id == recipient_id # 自分宛の返信は通知しない
+        # 親投稿のユーザー全員に通知を送信
+        parent_posts = self.ancestors
+        parent_posts.each do |parent_post|
+          recipient_id = parent_post.user_id
+          next if current_user.id == recipient_id # 自分宛の返信は通知しない
 
-        # 返信の通知を作成する
-        notification = current_user.sent_notifications.new(
-          recipient_id: parent_post.user_id, # 返信の親投稿のユーザーIDを通知の受信者とする
-          sender_id: current_user.id, # 通知の送信者
-          notifiable: self, # 返信
-          action: 'reply', # アクションタイプ
-          unread: true # 未読状態
-        )
-        notification.save if notification.valid?
+          # 返信の通知を作成する
+          notification = current_user.sent_notifications.new(
+            recipient_id: recipient_id, # 返信の親投稿のユーザーIDを通知の受信者とする
+            sender_id: current_user.id, # 通知の送信者
+            notifiable: self, # 返信
+            action: 'reply', # アクションタイプ
+            unread: true # 未読状態
+          )
+          notification.save if notification.valid?
 
-        # メール通知を非同期で送信
-        recipient = User.find(recipient_id)
-        return unless recipient.email_notify_on_reply
+          # メール通知を非同期で送信
+          recipient = User.find(recipient_id)
+          next unless recipient.email_notify_on_reply
 
-        UserMailer.reply_notification(recipient, self).deliver_later
+          UserMailer.reply_notification(recipient, self).deliver_later
+        end
       end
 
       # リポスト通知を作成するメソッド
