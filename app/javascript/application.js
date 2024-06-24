da4785faf1a6573dc1f97a721533ec5f8120a284
shota <s8152.45@gmail.com>
2024-06-24 15:07:11 +0900

    Fix: サービスワーカーの登録解除
    ログアウトや退会時にサービスワーカーの登録解除を行い、正しい画面遷移を保証するための修正を実施
    
    リンクに`data-turbo`属性を追加し、サービスワーカー解除スクリプトを動的に読み込むように変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,11 @@
+// app/javascript/application.js
 import "@hotwired/turbo-rails";
 import "controllers";
 import * as ActiveStorage from "@rails/activestorage";
 ActiveStorage.start();
 
-// サービスワーカーを登録するコードを追加
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    // ページロード後に一定時間待機してからサービスワーカーを登録
     setTimeout(() => {
       navigator.serviceWorker.register("/service-worker.js").then(
         (registration) => {
---
+++ public/unregister_service_worker.js
@@ -0,0 +1,8 @@
+// app/javascript/unregister_service_worker.js
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
