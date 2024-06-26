2a1ed4ca9101522280c26b0bc6bf3c733c5e869a
shota <s8152.45@gmail.com>
2024-06-26 09:42:07 +0900

    Fix: rubocop警告(hange_column メソッドを使用したマイグレーションはリバーシブルでないため)
    postsテーブルのdurationカラムをinteger型からdecimal型に変更するマイグレーションをリバーシブルに修正。upメソッドとdownメソッドを使用して、ロールバック時に元のinteger型に戻せるようにした。
    
    rails db:rollback後にマイグレーションファイルを修正し、再度マイグレーションを実行。

--- db/migrate/20240625211120_change_duration_to_decimal_in_posts.rb
+++ db/migrate/20240625211120_change_duration_to_decimal_in_posts.rb
@@ -1,5 +1,9 @@
 class ChangeDurationToDecimalInPosts < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :posts, :duration, :decimal, precision: 10, scale: 2
+  end
+
+  def down
+    change_column :posts, :duration, :integer
   end
 end
