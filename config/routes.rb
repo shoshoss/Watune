c8f4599e6c16967c8edf138f4b01fe4cb87f1129
shota <s8152.45@gmail.com>
2024-06-27 17:56:36 +0900

    Fix: sitemap に関連するコードを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
+  get '/sitemap.xml.gz', to: redirect('/public/sitemap.xml.gz')
+
   root 'static_pages#top'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'terms_of_use', to: 'static_pages#terms_of_use'
