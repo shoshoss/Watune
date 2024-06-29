3565e0c3a129fd767d7a65c0313f97df77eda6ad
shota <s8152.45@gmail.com>
2024-06-29 15:06:36 +0900

    Change:プロフィールの名前や表示の大きさ調整

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,20 +2,16 @@ module UsersHelper
   def following_button_class(location: nil)
     if location == :widget
       'min-w-[118px] ml-[12px]'
-    elsif params[:controller] == 'profiles' && params[:action] == 'show'
-      'min-w-[124px]'
     else
-      'min-w-[118px] ml-[12px]'
+      'min-w-[124px]'
     end
   end
 
   def follow_button_class(location: nil)
     if location == :widget
       'min-w-[90px] ml-[12px]'
-    elsif params[:controller] == 'profiles' && params[:action] == 'show'
-      'min-w-[94px]'
     else
-      'min-w-[90px] ml-[12px]'
+      'min-w-[94px]'
     end
   end
 end
