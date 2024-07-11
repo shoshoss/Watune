f061b6bcee67049212e225e291fc20f9aba9761a
shota <s8152.45@gmail.com>
2024-07-11 10:49:14 +0900

    Improve: @user.posts を使った共通部分と、bookmarked と liked を別に処理
    shared_with_you と posts_to_you のスコープを統一された形式へ

--- app/models/concerns/posts/shared_scopes.rb
+++ app/models/concerns/posts/shared_scopes.rb
@@ -3,11 +3,9 @@ module Posts
     extend ActiveSupport::Concern
 
     included do
-      # 共通部分の抽出
+      # direct_recipient の投稿を共通部分として取得するスコープ
       scope :direct_recipient_posts, lambda { |user|
-        joins(:post_users).where(
-          post_users: { user_id: user, role: 'direct_recipient' }
-        )
+        joins(:post_users).where(post_users: { user_id: user, role: 'direct_recipient' })
       }
 
       # 相互のダイレクトメッセージや複数選択の送受信、返信のやり取りを表示するスコープ
@@ -15,21 +13,20 @@ module Posts
         if current_user.nil? || profile_user.nil?
           Post.none
         else
-          direct_recipient_posts_to_profile_user = direct_recipient_posts(current_user).where(user_id: profile_user.id)
-          direct_recipient_posts_to_current_user = direct_recipient_posts(profile_user).where(user_id: current_user.id)
+          direct_to_profile_user = direct_recipient_posts(current_user).where(user_id: profile_user.id)
+          direct_to_current_user = direct_recipient_posts(profile_user).where(user_id: current_user.id)
 
           reply_from_current_user = where(
             user_id: current_user.id,
             post_reply_id: Post.where(user_id: profile_user.id).select(:id)
           )
-          # プロフィールユーザーが返信した投稿を取得
           reply_from_profile_user = where(
             user_id: profile_user.id,
             post_reply_id: Post.where(user_id: current_user.id).select(:id)
           )
 
-          Post.where(id: direct_recipient_posts_to_profile_user.select(:id))
-              .or(Post.where(id: direct_recipient_posts_to_current_user.select(:id)))
+          Post.where(id: direct_to_profile_user.select(:id))
+              .or(Post.where(id: direct_to_current_user.select(:id)))
               .or(Post.where(id: reply_from_current_user.select(:id)))
               .or(Post.where(id: reply_from_profile_user.select(:id)))
               .distinct
@@ -38,9 +35,12 @@ module Posts
 
       # 仲間からの投稿を取得するスコープ
       scope :posts_to_you, lambda { |user|
-        direct_recipient_posts(user)
-          .or(where(post_reply_id: Post.where(user_id: user).select(:id)))
-          .distinct
+        direct_posts = direct_recipient_posts(user)
+        reply_posts = where(post_reply_id: Post.where(user_id: user).select(:id))
+
+        Post.where(id: direct_posts.select(:id))
+            .or(Post.where(id: reply_posts.select(:id)))
+            .distinct
       }
     end
   end
