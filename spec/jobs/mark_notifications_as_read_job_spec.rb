0ea6549bf512f31a8e57358641d25f06b0cbf846
shota <s8152.45@gmail.com>
2024-07-14 21:09:37 +0900

    Remove: mark_notifications_as_read_job_spec.rb

--- spec/jobs/mark_notifications_as_read_job_spec.rb
+++ spec/jobs/mark_notifications_as_read_job_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe MarkNotificationsAsReadJob, type: :job do
+RSpec.describe MarkNotificationsAsReadJob do
   let(:user) { create(:user) }
-  let!(:notifications) { create_list(:notification, 3, user: user, unread: true) }
+  let!(:notifications) { create_list(:notification, 3, user:, unread: true) }
 
-  it "marks all notifications as read" do
-    expect {
+  it 'marks all notifications as read' do
+    expect do
       described_class.perform_now(user)
-    }.to change { Notification.where(unread: true).count }.from(3).to(0)
+    end.to change { Notification.where(unread: true).count }.from(3).to(0)
   end
 end
