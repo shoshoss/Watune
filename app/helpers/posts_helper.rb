63da4dadc9e21dde85fdc2443d9bd52250c041e8
shota <s8152.45@gmail.com>
2024-07-14 19:54:53 +0900

    Improve: rubocop
    CATEGORY_ICONS と CATEGORY_NAMES の2つの定数を導入 freeze メソッドを使用して、定数としての不変性を保証
    
    ハッシュから値を取得することで、case 文よりも簡潔で効率的なコードに

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,49 +1,33 @@
 module PostsHelper
+  CATEGORY_ICONS = {
+    'recommended' => 'fa fa-star text-yellow-400-accent',
+    'praise_gratitude' => 'fa fa-sun text-orange-400',
+    'music' => 'fa fa-music text-blue-500',
+    'child' => 'fa fa-smile-o text-pink-400',
+    'skill' => 'fa-solid fa-lightbulb text-yellow-400-accent',
+    'favorite' => 'fa fa-heart text-red-500',
+    'monologue' => 'fa fa-comment-dots text-teal-500',
+    'other' => 'fa fa-ellipsis-h text-gray-500'
+  }.freeze
+
+  CATEGORY_NAMES = {
+    'recommended' => 'おすすめ',
+    'praise_gratitude' => '褒めや感謝',
+    'music' => '音楽',
+    'child' => '子供の声',
+    'skill' => '特技や技術',
+    'favorite' => '好きなことや自己紹介',
+    'monologue' => 'ひとりごと',
+    'other' => 'その他'
+  }.freeze
+
   # カテゴリに対応するアイコンのクラスを返すメソッド
   def category_icon(category)
-    case category
-    when 'recommended'
-      'fa fa-star text-yellow-400-accent'
-    when 'praise_gratitude'
-      'fa fa-sun text-orange-400'
-    when 'music'
-      'fa fa-music text-blue-500'
-    when 'child'
-      'fa fa-smile-o text-pink-400'
-    when 'skill'
-      'fa-solid fa-lightbulb text-yellow-400-accent'
-    when 'favorite'
-      'fa fa-heart text-red-500'
-    when 'monologue'
-      'fa fa-comment-dots text-teal-500'
-    when 'other'
-      'fa fa-ellipsis-h text-gray-500'
-    else
-      'fa fa-circle text-gray-500'
-    end
+    CATEGORY_ICONS[category] || 'fa fa-circle text-gray-500'
   end
 
   # カテゴリ名を適切な形式に変換するメソッド
   def category_name(category)
-    case category
-    when 'recommended'
-      'おすすめ'
-    when 'praise_gratitude'
-      '褒めや感謝'
-    when 'music'
-      '音楽'
-    when 'child'
-      '子供の声'
-    when 'skill'
-      '特技や技術'
-    when 'favorite'
-      '好きなことや自己紹介'
-    when 'monologue'
-      'ひとりごと'
-    when 'other'
-      'その他'
-    else
-      category.titleize
-    end
+    CATEGORY_NAMES[category] || '不明なカテゴリー'
   end
 end
